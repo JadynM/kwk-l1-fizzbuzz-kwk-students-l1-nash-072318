d09b2337be21858917c443faa135d2b24b979581
JadynM <jadynmiller04@gmail.com>
2018-07-24 18:42:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -11,11 +11,11 @@ if number%
 
 number = 15
 
-if number%3 == 0
-  puts "fizzbuzz"
-elsif number%5 == 0
-  puts "buzz"
 else number%3 == 0
   puts "fizz"
+elsif number%5 == 0
+  puts "buzz"
+if number%3 == 0
+  puts "fizzbuzz"
 
 end
