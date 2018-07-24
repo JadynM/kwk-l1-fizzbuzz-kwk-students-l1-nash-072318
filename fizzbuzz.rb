6d8691ad295b49c86d507757c5d0f8745da066a5
JadynM <jadynmiller04@gmail.com>
2018-07-24 18:39:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,19 @@
 #when divisable by 5 buzz
 #when divisable by 15 fizzbuzz
 
+number = 3
+
+if number%
 
 
 
 
-
-number = 15
+number = 3
 
 if number%3 == 0
   puts "fizzbuzz"
+elsif number%5 == 0
+  puts "buzz"
+else number%3 == 0
+  puts "fizz"
 end
